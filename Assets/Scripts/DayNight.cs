f1229ff9c50cef2d4302581a5ae542dcb57bb4be
Eric <EricMouledoux@Gmail.com>
2015-09-10 18:04:56 -0500

    core mechanics

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNight : MonoBehaviour
+{
+	void Update ()
+    {
+        transform.Rotate((Time.deltaTime) ,0 ,0);
+	}
+}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -11,34 +11,55 @@ public class PlayerControls : MonoBehaviour
     public float m_speed = 1;
 
     public Text score;
+    public AudioSource crash;
 
     public Rigidbody rb;
 
-    public bool started;
+    float looseTimer, looseDelay;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         score.text = "0";
+        looseDelay = 3.0f;
     }
+
     void Update ()
     {
         rb.AddForce(transform.forward * m_speed * 100);
 
         if (Input.GetKey(m_left))
         {
-            rb.AddTorque(transform.up * -Time.deltaTime * 1000);
+            rb.AddTorque(transform.up * -Time.deltaTime * 1000 * m_speed);
         }
 
         if (Input.GetKey(m_right))
         {
-            rb.AddTorque(transform.up * +Time.deltaTime * 1000);
+            rb.AddTorque(transform.up * +Time.deltaTime * 1000 * m_speed);
         }
 
         score.text = ((int)Vector3.Distance(Vector3.zero, transform.position)).ToString() ;
-        m_speed += Time.deltaTime * 0.01f;
+        m_speed += Time.deltaTime * 0.03f;
 
-        if (rb.velocity.)
+        if (m_speed > 1.01 && Vector3.Distance(rb.velocity, Vector3.zero) < 0.1)
+        {
+            looseTimer += Time.deltaTime;
+        }
 
-        print(rb.velocity);
+        else
+        {
+            looseTimer = 0;
+        }
+
+        if (looseTimer > looseDelay)
+        {
+            print("What a looser");
+        }
+
+        //print(rb.velocity);
 	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        crash.Play();
+    }
 }
