6dd6e61c9a17ddf640f1bab91d32348656033448
Eric <EricMouledoux@Gmail.com>
2015-09-10 10:33:38 -0500

    Added all assets

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode m_left = KeyCode.A;
+    public KeyCode m_right = KeyCode.D;
+    public KeyCode m_jump = KeyCode.Space;
+
+    public float m_speed = 1;
+
+    void Update ()
+    {
+        transform.position += transform.forward * m_speed * Time.deltaTime;
+
+        if (Input.GetKey(m_left))
+        {
+            transform.position -= transform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(m_right))
+        {
+            transform.position += transform.right * Time.deltaTime;
+        }
+
+        m_speed += Time.deltaTime * 0.1f;
+	}
+}
---
+++ Assets/Scripts/SpawnLane.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpawnLane : MonoBehaviour
+{
+    public List<GameObject> lanes;
+
+    void SpawLane()
+    {
+        GameObject newLane = lanes[Random.Range(0, lanes.Count)];
+        Instantiate(newLane, transform.position + transform.forward * 10, transform.rotation);
+        transform.position += transform.forward * 10;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SpawLane();
+    }
+}
